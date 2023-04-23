ef57f36d6b5869d5ab5f528db4adf11ba565976a
王艺 <yi.wang@featuremaker.com>
2023-04-23 18:52:55 +0800

    2023年4月23日18:51:44

---
+++ app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="flex">
+            <div className="flex-initial">
+            </div>
+            <div className="flex-initial">
+            </div>
+            <div className="flex-initial">
+            </div>
+        </div>
+    );
+}
+
+export default App;
